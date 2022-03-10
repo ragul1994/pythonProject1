aaf99488f94006d72b2087853aefffd2e766fa42
ragulmanoharan1994 <ragulmanoharan1994@gmail.com>
2022-03-10 14:02:05 +0530

    Commit with comments

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import pyomo.environ as pyo
 from pyomo.environ import *
 from pyomo.opt import SolverFactory
+# Solver Factory Using IPOPT For NLP
+
 model = pyo.ConcreteModel()
 model.x = pyo.Var(bounds=(0, 10))
 model.y = pyo.Var(bounds=(0, 10))
 x = model.x
 y = model.y
-model.C1 = pyo.Constraint(expr=-x+2*y*x<=8)
-model.C2 = pyo.Constraint(expr=2*x+y<=14)
-model.C3 = pyo.Constraint(expr=2*x-y<=10)
-model.obj = pyo.Objective(expr=x+y*x, sense=maximize)
+model.C1 = pyo.Constraint(expr=-x + 2 * y * x <= 8)
+model.C2 = pyo.Constraint(expr=2 * x + y <= 14)
+model.C3 = pyo.Constraint(expr=2 * x - y <= 10)
+model.obj = pyo.Objective(expr=x + y * x, sense=maximize)
 opt = SolverFactory('ipopt', executable='C:\\Ipopt-3.10.1-win64-intel11.1\\bin\\ipopt.exe')
 opt.solve(model)
 model.pprint()
@@ -17,3 +19,4 @@ x_value = pyo.value(x)
 y_value = pyo.value(y)
 print('x=', x_value)
 print('y=', y_value)
+# Comments has been added
